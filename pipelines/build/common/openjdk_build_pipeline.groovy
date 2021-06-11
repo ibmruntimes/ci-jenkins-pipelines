86479f7d883c6147b02be6defdc879805b5e4598
Adam Brousseau <adam.brousseau88@gmail.com>
2021-06-11 15:49:37 -0400

    Remove variant (openj9) from binary filename
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -1019,7 +1019,7 @@ class Build {
 
         javaToBuild = javaToBuild.toUpperCase()
 
-        def fileName = "Semeru${javaToBuild}-jdk_${architecture}_${os}_${variant}"
+        def fileName = "Semeru${javaToBuild}-jdk_${architecture}_${os}"
 
         if (additionalFileNameTag) {
             fileName = "${fileName}_${additionalFileNameTag}"
