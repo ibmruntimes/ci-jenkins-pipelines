e4c7c926888922b7397bd175b0fa44b2cbeed65d
Surya Narkedimilli <snarkedi@in.ibm.com>
2021-09-21 17:10:03 +0530

    Adding JDK17 ISR CE specs

--- pipelines/jobs/configurations/jdk17_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk17_pipeline_config.groovy
@@ -18,6 +18,18 @@ class Config17 {
                 configureArgs        : '--with-noncompressedrefs --enable-dtrace'
         ],
 
+        x64MacIBM    : [
+            os                  : 'mac',
+            arch                : 'x64',
+            additionalNodeLabels : 'ci.project.openj9 && hw.arch.x86 && sw.os.osx.10_14',
+            test                : 'default',
+            configureArgs       : [
+                    "openj9"      : '--enable-dtrace=auto  --with-vendor-name="International Business Machines Corporation" --with-vendor-url=https://www.ibm.com/ --with-vendor-bug-url=https://github.com/eclipse-openj9/openj9/issues --with-vendor-vm-bug-url=https://github.com/eclipse-openj9/openj9/issues'
+            ],
+            additionalFileNameTag: "IBM",
+            buildArgs : "--ssh --disable-adopt-branch-safety -r git@github.ibm.com:runtimes/openj9-openjdk-jdk17 -b ibm_sdk"
+        ],
+
         x64Linux  : [
                 os                  : 'linux',
                 arch                : 'x64',
@@ -64,6 +76,24 @@ class Config17 {
                 test                : 'default'
         ],
 
+        x64LinuxIBM  : [
+            os                  : 'linux',
+            arch                : 'x64',
+            additionalNodeLabels : 'ci.project.openj9 && hw.arch.x86 && sw.os.linux',
+            dockerImage         : 'adoptopenjdk/centos6_build_image@sha256:19cdb5284da031aef7c73cb52ee7018502d65d0ca21cebc45d9652eec3926458',
+            dockerFile: [
+                    "openj9"  : 'pipelines/build/dockerFiles/cuda.dockerfile'
+            ],
+            dockerNode          : 'sw.tool.docker && sw.config.uid1000',
+            dockerCredential    : '9f50c848-8764-440d-b95a-1d295c21713e',
+            test                : 'default',
+            configureArgs       : [
+                    "openj9"      : '--enable-jitserver --enable-dtrace=auto --with-vendor-name="International Business Machines Corporation" --with-vendor-url=https://www.ibm.com/ --with-vendor-bug-url=https://github.com/eclipse-openj9/openj9/issues --with-vendor-vm-bug-url=https://github.com/eclipse-openj9/openj9/issues'
+            ],
+            additionalFileNameTag: "IBM",
+            buildArgs : "--ssh --disable-adopt-branch-safety -r git@github.ibm.com:runtimes/openj9-openjdk-jdk17 -b ibm_sdk"
+        ],
+
         x64Windows: [
                 os                  : 'windows',
                 arch                : 'x64',
@@ -80,6 +110,22 @@ class Config17 {
                 test                 : 'default',
                 additionalFileNameTag: "windowsXL",
                 configureArgs        : '--with-noncompressedrefs'
+        ],
+
+        x64WindowsIBM: [
+            os                  : 'windows',
+            arch                : 'x64',
+            additionalNodeLabels: [
+                    openj9:     'ci.project.openj9 && hw.arch.x86 && sw.os.windows'
+            ],
+            buildArgs : [
+                    openj9 : "--ssh --disable-adopt-branch-safety -r git@github.ibm.com:runtimes/openj9-openjdk-jdk17 -b ibm_sdk"
+            ],
+            test                : 'default',
+            configureArgs       : [
+                    "openj9"      : '--with-vendor-name="International Business Machines Corporation" --with-vendor-url=https://www.ibm.com/ --with-vendor-bug-url=https://github.com/eclipse-openj9/openj9/issues --with-vendor-vm-bug-url=https://github.com/eclipse-openj9/openj9/issues --with-jdk-rc-name="IBM Semeru Runtime"'
+            ],
+            additionalFileNameTag: "IBM"
         ],
 
         // TODO: Enable testing (https://github.com/adoptium/ci-jenkins-pipelines/issues/77)
@@ -120,6 +166,19 @@ class Config17 {
                 cleanWorkspaceAfterBuild: true
         ],
 
+        ppc64AixIBM    : [
+            os                  : 'aix',
+            arch                : 'ppc64',
+            additionalNodeLabels: [
+                    openj9:  'hw.arch.ppc64 && sw.os.aix.7_1'
+            ],
+            test                : 'default',
+            configureArgs       : [
+                    "openj9"      : '--with-vendor-name="International Business Machines Corporation" --with-vendor-url=https://www.ibm.com/ --with-vendor-bug-url=https://github.com/eclipse-openj9/openj9/issues --with-vendor-vm-bug-url=https://github.com/eclipse-openj9/openj9/issues'
+            ],
+            additionalFileNameTag: "IBM",
+            buildArgs : "--ssh --disable-adopt-branch-safety -r git@github.ibm.com:runtimes/openj9-openjdk-jdk17 -b ibm_sdk"
+        ],
 
         s390xLinux    : [
                 os                  : 'linux',
@@ -130,6 +189,18 @@ class Config17 {
                         openj9:  'hw.arch.s390x && (sw.os.cent.7 || sw.os.rhel.7)'
                 ],
                 configureArgs       : '--enable-dtrace --with-vendor-name="IBM Corporation" --with-vendor-version-string="17.0.0.0-m1" --with-product-name="IBM Semeru Runtime" --with-product-suffix="Open Edition" --with-vendor-url=https://www.ibm.com/ --with-vendor-bug-url=https://github.com/eclipse-openj9/openj9/issues --with-vendor-vm-bug-url=https://github.com/eclipse-openj9/openj9/issues'
+        ],
+
+        s390xLinuxIBM    : [
+            os                  : 'linux',
+            arch                : 's390x',
+            test                : 'default',
+            additionalNodeLabels: [
+                    openj9:  'hw.arch.s390x && (sw.os.cent.7 || sw.os.rhel.7)'
+            ],
+                configureArgs       : '--enable-dtrace=auto --with-vendor-name="International Business Machines Corporation" --with-vendor-url=https://www.ibm.com/ --with-vendor-bug-url=https://github.com/eclipse-openj9/openj9/issues --with-vendor-vm-bug-url=https://github.com/eclipse-openj9/openj9/issues',
+            additionalFileNameTag: "IBM",
+            buildArgs : "--ssh --disable-adopt-branch-safety -r git@github.ibm.com:runtimes/openj9-openjdk-jdk17 -b ibm_sdk"
         ],
 
         s390xLinuxXL  : [
@@ -163,6 +234,21 @@ class Config17 {
                 test                 : 'default',
                 additionalFileNameTag: "linuxXL",
                 configureArgs        : '--with-noncompressedrefs --disable-ccache --enable-dtrace'
+        ],
+
+        ppc64leLinuxIBM    : [
+            os                  : 'linux',
+            arch                : 'ppc64le',
+            additionalNodeLabels : 'centos7',
+            test                : 'default',
+            additionalNodeLabels: [
+                    openj9:  'hw.arch.ppc64le && (sw.os.cent.7 || sw.os.rhel.7)'
+            ],
+            configureArgs       : [
+                        "openj9"      : '--enable-dtrace=auto --enable-jitserver --with-vendor-name="International Business Machines Corporation" --with-vendor-url=https://www.ibm.com/ --with-vendor-bug-url=https://github.com/eclipse-openj9/openj9/issues --with-vendor-vm-bug-url=https://github.com/eclipse-openj9/openj9/issues'
+            ],
+            additionalFileNameTag: "IBM",
+            buildArgs : "--ssh --disable-adopt-branch-safety -r git@github.ibm.com:runtimes/openj9-openjdk-jdk17 -b ibm_sdk"
         ],
 
         aarch64Linux    : [
