d72823141fd19e623979b5fcac2343fcae99ed5c
julian55455 <julianatuhwera55@gmail.com>
2022-10-18 13:20:24 +0300

    CUSTOMIZED_SDK_URL should only be used when SDK_RESOURCE=customized

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -444,6 +444,7 @@ class Build {
                                     parameters: [
                                             //context.string(name: 'UPSTREAM_JOB_NUMBER', value: "${env.BUILD_NUMBER}"),
                                             //context.string(name: 'UPSTREAM_JOB_NAME', value: "${env.JOB_NAME}"),
+                                            context.string(name: 'SDK_RESOURCE', value: "customized"),
                                             context.string(name: 'CUSTOMIZED_SDK_URL', value: artifactsUrls),
                                             context.string(name: 'CUSTOMIZED_SDK_URL_CREDENTIAL_ID', value: artifactoryCredential),
                                             context.string(name: 'RELEASE_TAG', value: "${buildConfig.SCM_REF}"),
