75dd254c1e1a52030e035d574a10711f50486d92
Aswin K R <aswinkr77@gmail.com>
2025-11-20 10:31:07 +0530

    Fix s390x linux docker image path hierarchy in jdk11
    Signed-off-by: Aswin K R <aswinkr77@gmail.com>

--- pipelines/jobs/Semeru_configuration/jdk11u_pipeline_config.groovy
+++ pipelines/jobs/Semeru_configuration/jdk11u_pipeline_config.groovy
@@ -83,7 +83,7 @@ class Config11 {
             additionalNodeLabels: [
                     openj9:  'ci.project.openj9 && hw.arch.s390x'
             ],
-            dockerImage: 'runtimes/runtimes/semeru/s390_rhel7_build_image',
+            dockerImage: 'runtimes/semeru/s390_rhel7_build_image',
             dockerRegistry: 'https://icr.io/',
             dockerCredential : 'ea4b3e98-4334-48d4-8211-98deb8767b55',
             dockerNode : 'sw.tool.docker',
@@ -364,7 +364,7 @@ class Config11 {
             additionalNodeLabels: [
                     openj9:  'ci.project.openj9 && hw.arch.s390x'
             ],
-            dockerImage: 'runtimes/runtimes/semeru/s390_rhel7_build_image',
+            dockerImage: 'runtimes/semeru/s390_rhel7_build_image',
             dockerRegistry: 'https://icr.io/',
             dockerCredential : 'ea4b3e98-4334-48d4-8211-98deb8767b55',
             dockerNode : 'sw.tool.docker',
